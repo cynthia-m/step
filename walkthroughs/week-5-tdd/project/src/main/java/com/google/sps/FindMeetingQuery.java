6b733f66cf5ff2e9bd715acced10c492bd057977
Cynthia Ma <cwjjma@gmail.com>
2020-06-19 13:41:41 +0000

    Implement FindMeetingQuery

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -1,3 +1,4 @@
+
 // Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,9 +16,94 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    Collection<String> attendees = request.getAttendees();
+    int reqDuration = (int) request.getDuration();
+    Set<String> test2 = new HashSet<> ();
+    test2.addAll(attendees);
+    Collection<TimeRange> ans = new ArrayList<TimeRange> ();
+    Collection<TimeRange> eventsTR = new ArrayList<TimeRange> ();
+    for (Iterator currEvent = events.iterator(); currEvent.hasNext(); ){ 
+        eventsTR.add((TimeRange) ((Event)currEvent.next()).getWhen());
+    }
+
+    int start = 0;
+    int end = 0;
+    int i = 0;
+    TimeRange t = TimeRange.fromStartEnd(start, end, true);
+    while (start<=1439 && end<=1439) {
+      if(checkOverlapTime(t, eventsTR)){
+        if(start == end){
+          start++;
+          end++;
+        }
+        else{
+          if (checkOverlapAttendeesHelp(t, test2, events)) {
+            if(end-start>=reqDuration){
+              t = TimeRange.fromStartEnd(start, end-1, true);
+              ans.add(t);
+            }
+            start = end;
+          }
+          else{
+            end++;
+          }
+        }
+      }
+      else{
+        end++;
+      }
+      t = TimeRange.fromStartEnd(start, end, true);
+    }
+    
+    if(end-start>reqDuration){
+      t = TimeRange.fromStartEnd(start, end-1, true);
+        ans.add(t);
+    }
+    return ans;
+
+  }
+  //checks if a timerange overlaps any of the time ranges
+  public boolean checkOverlapTime(TimeRange t1, Collection <TimeRange> t2){
+
+    for (Iterator currEvent = t2.iterator(); currEvent.hasNext(); ){ 
+      TimeRange t = (TimeRange) currEvent.next();
+      if(t1.overlaps(t)){
+        return true;
+      }
+    }
+    return false;
+
+  }
+
+  public boolean checkOverlapAttendeesHelp(TimeRange t, Set<String> a1, Collection<Event> events){
+    Event e;
+    for (Iterator currEvent = events.iterator(); currEvent.hasNext(); ){ 
+      e = (Event) currEvent.next();
+      if(t.overlaps(e.getWhen()) && checkOverlapAttendees(a1, e.getAttendees())){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean checkOverlapAttendees(Set<String> a1, Set<String> a2){
+     for(String e1 : a1){
+        for(String e2 : a2){
+          if(e1.equals(e2)){
+            return true;
+          }
+        }
+     }
+
+    return false;
+
   }
 }
